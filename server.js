8fda78073130ffbdc013b644aaa1eebee951810e
GabrielProzin <gabrielalexandrino2004@gmail.com>
2024-11-19 21:09:44 -0300

    Initial commit: Added base project structure and essential files

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+const posts = [
+  {
+    id: 1,
+    descricao: "uma foto teste",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Gato fofo dormindo",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 3,
+    descricao: "Gato brincando com um novelo de lã",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+];
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Servidor escutando...");
+});
+
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
+});
