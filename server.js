1de4fbfcfba8030c32d9983cdf61ba66fa761d47
GabrielProzin <gabrielalexandrino2004@gmail.com>
2024-11-20 16:20:31 -0300

    Updating my repository with new files

--- server.js
+++ server.js
@@ -1,39 +1,10 @@
 import express from "express";
-
-const posts = [
-  {
-    id: 1,
-    descricao: "uma foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato fofo dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Gato brincando com um novelo de lã",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+
+routes(app);
+
 app.listen(3000, () => {
   console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
