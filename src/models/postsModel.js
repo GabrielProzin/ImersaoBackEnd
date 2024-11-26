307b68673bf5a4817766bce2475cc3ed623fb50d
GabrielProzin <gabrielalexandrino2004@gmail.com>
2024-11-25 23:14:34 -0300

    Deploying my project :)

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 import { postarNovoPost } from "../controllers/postsController.js";
 
@@ -13,4 +14,11 @@ export async function criarPost(novoPost) {
   const db = conexao.db("imersaoBackEnd");
   const colecao = db.collection("posts");
   return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+  const db = conexao.db("imersaoBackEnd");
+  const colecao = db.collection("posts");
+  const objID = ObjectId.createFromHexString(id);
+  return colecao.updateOne({ _id: new ObjectId(objID) }, { $set: novoPost });
 }
